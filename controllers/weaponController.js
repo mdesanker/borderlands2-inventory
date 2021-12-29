49c7b8745b17df15e1e23bc66b368fd9d47e5b1b
Michael Desanker <mdesanker@gmail.com>
2021-12-29 12:57:40 -0500

    Add rough navbar

--- controllers/weaponController.js
+++ controllers/weaponController.js
@@ -2,11 +2,7 @@ const Weapon = require("../models/weapon");
 
 // Display home page
 exports.index = function (req, res, next) {
-  // res.send("NOT IMPLEMENTED: Site home page");
-  res.render("index", {
-    title: "Borderlands 2 - Database",
-    subtitle: "Weapons List",
-  });
+  res.render("index", { title: "Borderlands 2" });
 };
 
 // Display list of all weapons
