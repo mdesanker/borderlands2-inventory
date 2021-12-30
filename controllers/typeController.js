16572b16355b920f7537b693848077a4e7161f7d
Michael Desanker <mdesanker@gmail.com>
2021-12-30 10:58:26 -0500

    Add type detail controller and view

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -1,4 +1,7 @@
 const Type = require("../models/type");
+const Weapon = require("../models/weapon");
+
+const async = require("async");
 
 // Display list of all types
 exports.typeList = async function (req, res, next) {
@@ -12,6 +15,26 @@ exports.typeList = async function (req, res, next) {
 };
 
 // Display detail for specific type
-exports.typeDetail = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: Type detail: " + req.params.id);
+exports.typeDetail = async function (req, res, next) {
+  let typeDetail;
+  try {
+    typeDetail = await async.parallel({
+      type: function (cb) {
+        Type.findById(req.params.id, cb);
+      },
+      weapons: function (cb) {
+        Weapon.find({ type: req.params.id }, cb).sort({ name: 1 });
+      },
+    });
+  } catch (err) {
+    return next(err);
+  }
+  // Type not found
+  if (typeDetail.type == null) {
+    const err = new Error("Type not found");
+    type.status = 404;
+    return next(err);
+  }
+  // Successful, so render
+  res.render("typeDetail", { title: "Type Detail", detail: typeDetail });
 };
