382d9b0cf73bd8a141da6fbe320abd55b1b4d594
Michael Desanker <mdesanker@gmail.com>
2021-12-30 10:51:25 -0500

    Add rarity detail controller and view

--- controllers/rarityController.js
+++ controllers/rarityController.js
@@ -1,4 +1,7 @@
 const Rarity = require("../models/rarity");
+const Weapon = require("../models/weapon");
+
+const async = require("async");
 
 // Display list of all rarities
 exports.rarityList = async function (req, res, next) {
@@ -12,6 +15,26 @@ exports.rarityList = async function (req, res, next) {
 };
 
 // Display details for specific rarity
-exports.rarityDetail = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: Rarity detail: " + req.params.id);
+exports.rarityDetail = async function (req, res, next) {
+  let rarityDetail;
+  try {
+    rarityDetail = await async.parallel({
+      rarity: function (cb) {
+        Rarity.findById(req.params.id, cb);
+      },
+      weapons: function (cb) {
+        Weapon.find({ rarity: req.params.id }, cb).sort({ name: 1 });
+      },
+    });
+  } catch (err) {
+    return next(err);
+  }
+  // Rarity not found
+  if (rarityDetail.rarity == null) {
+    const err = new Error("Rarity not found");
+    err.status = 404;
+    return next(err);
+  }
+  // Successful, so render
+  res.render("rarityDetail", { title: "Rarity Detail", detail: rarityDetail });
 };
